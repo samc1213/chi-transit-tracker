fc8052fcf117464dc2084bc31bf56156acf6fd47
Sam Cohen <samc1213@gmail.com>
2020-02-17 16:41:29 -0600

    Updates

---
+++ precache-manifest.5606565549f8a68cd6649822671f5bf4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b17fc7351803c78225c6015e0250bff2",
+    "url": "/chi-transit-tracker/index.html"
+  },
+  {
+    "revision": "6efcd817333258a3a8b9",
+    "url": "/chi-transit-tracker/static/css/main.5a11830b.chunk.css"
+  },
+  {
+    "revision": "f2d6d84c176a73ca52a6",
+    "url": "/chi-transit-tracker/static/js/2.bb0c16f8.chunk.js"
+  },
+  {
+    "revision": "6efcd817333258a3a8b9",
+    "url": "/chi-transit-tracker/static/js/main.d2e74aba.chunk.js"
+  },
+  {
+    "revision": "fe2a97ce5a20d8c3e59f",
+    "url": "/chi-transit-tracker/static/js/runtime~main.c441f3fa.js"
+  }
+]);
