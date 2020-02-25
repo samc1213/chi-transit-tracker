0e49b29dd619ac5f31246d6ea555d5c49acbf35c
Sam Cohen <samc1213@gmail.com>
2020-02-24 20:01:26 -0600

    Updates

--- precache-manifest.bfc7dc3bd7a3246d9bdbdf4ddbd7a9ed.js
+++ precache-manifest.bfc7dc3bd7a3246d9bdbdf4ddbd7a9ed.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "50fc8e6e2da27633c9d0e4bdd588cff3",
+    "revision": "be952e54cd249e7456deb289c09e899a",
     "url": "/chi-transit-tracker/index.html"
   },
   {
-    "revision": "c9feac8af178d9e77c5b",
+    "revision": "30521158e4ce7d577ad0",
     "url": "/chi-transit-tracker/static/css/main.5a11830b.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/chi-transit-tracker/static/js/2.bb0c16f8.chunk.js"
   },
   {
-    "revision": "c9feac8af178d9e77c5b",
-    "url": "/chi-transit-tracker/static/js/main.091acafe.chunk.js"
+    "revision": "30521158e4ce7d577ad0",
+    "url": "/chi-transit-tracker/static/js/main.fcd1015b.chunk.js"
   },
   {
     "revision": "fe2a97ce5a20d8c3e59f",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/chi-transit-tracker/precache-manifest.6fe7c4f70def680ac8523ad2397f1ee7.js"
+  "/chi-transit-tracker/precache-manifest.bfc7dc3bd7a3246d9bdbdf4ddbd7a9ed.js"
 );
 
 self.addEventListener('message', (event) => {
